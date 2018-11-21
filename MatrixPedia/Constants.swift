5537e862d210a5fe2ba07564ca40f8f8bbcdea50
Alex Cros <alexcroscuadros@gmail.com>
2018-11-21 23:38:03 +0100

    constants

---
+++ MatrixPedia/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MatrixPedia
+//
+//  Created by Alexandre on 21/11/18.
+//  Copyright © 2018 Alexandre. All rights reserved.
+//
+
+import Foundation
+
+class Constants{
+    
+    public static let URL = "http://127.0.0.1/characters";
+    
+}
--- MatrixPedia/modules/Interactor/MatrixCharactersInteractor.swift
+++ MatrixPedia/modules/Interactor/MatrixCharactersInteractor.swift
@@ -16,11 +16,12 @@ class MatrixCharactersInteractor: PresentorToInterectorProtocol{
     // TODO: PASAR LA VISTA AL PRESENTER YA QUE EN LA MASTERCLASS
     // DE VIPER LOS DATOS LOS PASABA DIRECTAMENTE DESDE EL TVC
     func fetchMatrixCharacters() {
-        Alamofire.request("http://127.0.0.1/characters").responseJSON { [weak self] response in
+        Alamofire.request(Constants.URL).responseJSON { [weak self] response in
             
             if let data = response.data,
                 let jsonData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
                 let characters = jsonData as? [[String : Any]] {
+               
                 var newCharacters = [MatrixCharacter]()
                 
                 for character in characters {
@@ -29,6 +30,9 @@ class MatrixCharactersInteractor: PresentorToInterectorProtocol{
                     }
                     newCharacters.append(newCharacter)
                 }
+                
+                self?.presenter?.matrixCharactersFetched(characters: newCharacters.first!)
+                
                 // pasar a presenter
 //                self?.characters = newCharacters
 //                self?.tableView.reloadData()
